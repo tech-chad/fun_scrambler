57951a241a15ed80da96980a30bb2e5a9bbf2444
chad <techchad2@gmail.com>
2021-11-05 11:23:45 -0500

    Added cli and test_mode

--- fun_scrambler.py
+++ fun_scrambler.py
@@ -1,9 +1,12 @@
 # fun-scrambler
+import argparse
 import curses
 import random
 import time
 
 from typing import Optional
+from typing import Sequence
+
 
 char_set = [x for x in "ABCDEFGHIJKLMNOPQRTSTUVWXYZabcdefghijklmnopqrstuvwxyz 0123456789"]
 curses_color = {"green": curses.COLOR_GREEN, "red": curses.COLOR_RED,
@@ -25,7 +28,8 @@ def set_curses_color(fg_color: Optional[str] = "white", option: Optional[str] = 
         curses.init_pair(8, color, curses.COLOR_BLACK)
 
 
-def scrambler_loop(screen):
+def scrambler_loop(screen, args):
+    all_char_set = ["T"] if args.test_mode else char_set
     curses.curs_set(0)  # Set the cursor to off.
     screen.timeout(0)  # Turn blocking off for screen.getch().
 
@@ -40,7 +44,7 @@ def scrambler_loop(screen):
                 r = random.randint(0, 10)
                 if r >= 6:
                     screen.addstr(y, x,
-                                  random.choice(char_set),
+                                  random.choice(all_char_set),
                                   curses.color_pair(random_color))
         time.sleep(0.1)
 
@@ -52,8 +56,16 @@ def scrambler_loop(screen):
     screen.refresh()
 
 
+def argument_parsing(agv: Optional[Sequence] = None):
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--test_mode", action="store_true", help=argparse.SUPPRESS)
+    return parser.parse_args(agv)
+
+
 def main():
-    curses.wrapper(scrambler_loop)
+    args = argument_parsing()
+    curses.wrapper(scrambler_loop, args)
 
 
 if __name__ == "__main__":
